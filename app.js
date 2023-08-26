d8c693eeb3872c554632541351aff9a9576b2f45
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-08-26 19:57:49 +0530

    added ejs view engine

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`)
+});
